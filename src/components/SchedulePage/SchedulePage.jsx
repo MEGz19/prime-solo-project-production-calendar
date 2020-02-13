73a9afebc956476e18455ee9ac3f87e2c0fe6456
Meghan Gunderson <megundersonmn@gmail.com>
2020-02-13 13:20:19 -0600

    added protected route in App.js to /schedule

---
+++ src/components/SchedulePage/SchedulePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+
+
+
+
+
+
+
+
+
+
+// this allows us to use <App /> in index.js
+export default connect(mapStateToProps)(UserPage);
