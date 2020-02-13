00a01d6698481743e8500bc8301969f381a775b7
Meghan Gunderson <megundersonmn@gmail.com>
2020-02-13 13:34:25 -0600

    set up basic framework of schedulepage component, but not showing on DOM yet

--- src/components/SchedulePage/SchedulePage.jsx
+++ src/components/SchedulePage/SchedulePage.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 
+class SchedulePage extends Component {
+    render(){
+        return(
+            <p>This is the schedule page.</p>
+
+
+        )
+    }
+}
 
 
 
 
 
-
-
+// reduxState is our entire store
+const mapStateToProps = (reduxState) => {
+    // this will make ALL reducers available on this.props.reduxState
+    return {reduxState}
+  }
 
 
 // this allows us to use <App /> in index.js
-export default connect(mapStateToProps)(UserPage);
+export default connect(mapStateToProps)(SchedulePage);
