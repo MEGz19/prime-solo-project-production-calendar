536643eba1a28d46582927f08bc05a321c6834c8
Meghan Gunderson <megundersonmn@gmail.com>
2020-02-14 16:02:34 -0600

    added date, start, and end time fields to schedule page

--- src/components/SchedulePage/SchedulePage.jsx
+++ src/components/SchedulePage/SchedulePage.jsx
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import DatePicker from "react-datepicker";
+
+
 
 // Material UI Imports
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import PropTypes from 'prop-types';
 
 
 class SchedulePage extends Component {
-    DatePickers(props) {
-        const { classes } = props;
-        DatePickers.propTypes = {
-            classes: PropTypes.object.isRequired,
-          };
+    // Part of Mat. UI functionality
+    // DatePickers(props) {
+    //     const { classes } = props;
+    //     DatePickers.propTypes = {
+    //         classes: PropTypes.object.isRequired,
+    //       }; //END MAT UI.
+    // START REACT-DATEPICKER
+    // state = {
+    //     startDate: new Date()
+    // };
 
+
+    // handleChange = date => {
+    //     this.setState({
+    //         startDate: date
+    //     });
+    // }; //END REACT DATEPICKER
 
     render() {
         return (
@@ -22,6 +37,32 @@ class SchedulePage extends Component {
                     <p>The rehearsal period for <i>Wicked</i> will be from May 1 to May 28, 2020. Performances will be from May 29 through May 14, 2020. Please enter any conflicts, or times you are <b>NOT</b> available during this time frame.</p>
                 </div>
                 <div>
+                {/* DATE INPUT  */}
+                    <input type="date" id="date" name="date" label="date" />
+                </div>
+                {/* START TIME INPUT  */}
+                <div>
+                    <input type="time" id="startTime" name="startTime" />
+                </div>
+                 {/* END TIME INPUT  */}
+                 <div>
+                    <input type="time" id="endTime" name="endTime" />
+                </div>
+
+
+
+
+
+                {/* <div> */}
+                {/* REACT DATEPICKER START */}
+                {/* <DatePicker
+                    selected={this.state.startDate}
+                    onChange={this.handleChange}
+                /> */}
+                {/* REACT DATEPICKER END */}
+
+                {/* MATERIAL UI START */}
+                {/* <div>
                     <form className={classes.container} noValidate>
                         <TextField
                             id="date"
@@ -35,7 +76,8 @@ class SchedulePage extends Component {
                         />
                     </form>
                     );
-                </div>
+                </div> */}
+                {/* END MATERIAL UI */}
             </div>
         )
     }
