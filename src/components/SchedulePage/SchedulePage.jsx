b085cba34b383161c6cf76baacff886e96d8ef2c
Meghan Gunderson <megundersonmn@gmail.com>
2020-02-14 11:45:54 -0600

    added instructions to schedule page

--- src/components/SchedulePage/SchedulePage.jsx
+++ src/components/SchedulePage/SchedulePage.jsx
@@ -3,10 +3,12 @@ import { connect } from 'react-redux';
 
 
 class SchedulePage extends Component {
-    render(){
-        return(
-            <p>This is the schedule page.</p>
-
+    render() {
+        return (
+            <div>
+                <h1>Schedule Page</h1>
+                <p>The rehearsal period for <i>Wicked</i> will be from May 1 to May 28, 2020. Performances will be from May 29 through May 14, 2020. Please enter any conflicts, or times you are <b>NOT</b> available during this time frame.</p>
+            </div>
 
         )
     }
@@ -19,8 +21,8 @@ class SchedulePage extends Component {
 // reduxState is our entire store
 const mapStateToProps = (reduxState) => {
     // this will make ALL reducers available on this.props.reduxState
-    return {reduxState}
-  }
+    return { reduxState }
+}
 
 
 // this allows us to use <App /> in index.js
