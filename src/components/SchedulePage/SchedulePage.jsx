9d958f67f5de443b500fa9a1d1bd2357b4564fe8
Meghan Gunderson <megundersonmn@gmail.com>
2020-02-14 12:01:05 -0600

    added date picker form, but not working yet

--- src/components/SchedulePage/SchedulePage.jsx
+++ src/components/SchedulePage/SchedulePage.jsx
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+// Material UI Imports
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
 
 class SchedulePage extends Component {
+    DatePickers(props) {
+        const { classes } = props;
+        DatePickers.propTypes = {
+            classes: PropTypes.object.isRequired,
+          };
+
+
     render() {
         return (
             <div>
-                <h1>Schedule Page</h1>
-                <p>The rehearsal period for <i>Wicked</i> will be from May 1 to May 28, 2020. Performances will be from May 29 through May 14, 2020. Please enter any conflicts, or times you are <b>NOT</b> available during this time frame.</p>
+                <div>
+                    <h1>Schedule Page</h1>
+                    <p>The rehearsal period for <i>Wicked</i> will be from May 1 to May 28, 2020. Performances will be from May 29 through May 14, 2020. Please enter any conflicts, or times you are <b>NOT</b> available during this time frame.</p>
+                </div>
+                <div>
+                    <form className={classes.container} noValidate>
+                        <TextField
+                            id="date"
+                            label="Birthday"
+                            type="date"
+                            defaultValue="2017-05-24"
+                            className={classes.textField}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                    </form>
+                    );
+                </div>
             </div>
-
         )
     }
 }
