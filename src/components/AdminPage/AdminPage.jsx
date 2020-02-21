b810495572a01799d60c1f1759d6017655719c44
Meghan Gunderson <megundersonmn@gmail.com>
2020-02-21 01:31:34 -0600

    started updating admin page GET route

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
+import ConflictList from "../ConflictList/ConflictList"
+
 
 class AdminPage extends Component {
+    componentDidMount () {
+        //GET request to SAGA
+        this.props.dispatch({type: 'GET_ALL_CONFLILCTS'})
+    }
 
 
     render() {
         return (
-            <>
-                <p>This is the Admin Page.</p>
-            </>
+            <div>
+                <div>
+                    <h1>Administrator Page</h1>
+                </div> 
+                <div>
+                    {/* RENDER ALL CONFLICTS TO DOM */}
+                    <ul>
+                        {this.props.reduxState.schedule.map(conflictItem => 
+                             
+                             <ConflictList key={conflictItem.id} conflictItem={conflictItem} />
+                        )}    
+                    </ul>
+                </div>   
+            </div>
         )
     }
-
 }
+    
 
-// reduxState is our entire store
+    
+
+{/* // reduxState is our entire store */}
 const mapStateToProps = (reduxState) => ({
     reduxState
 })
